68c921584481fb18d002c74d73d5b20fd543d79f
yosefhayim <yosefisabag+03@gmail.com>
2025-05-08 23:37:20 +0300

    continue tomoorow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import Homepage from "./Pages/Homepage/Homepage";
+import Navbar from "./Components/Navbar/Navbar";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
