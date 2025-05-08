fd79b177f72d1cdd9a36feb852bac47d7f2d3ca9
yosefhayim <yosefisabag+03@gmail.com>
2025-05-08 23:09:53 +0300

    init nav

---
+++ client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+  return (
+    <nav aria-label="Main navigation">
+      <ul>
+        <li>
+          <a href="#reserve">Reserve Me</a>
+        </li>
+        <li>
+          <a href="#business-types">Business Types</a>
+        </li>
+        <li>
+          <a href="#support">Information and Support</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
+        <li>
+          <a href="#price">Price</a>
+        </li>
+        <li>
+          <a href="">Start Now</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/Pages/NewBusiness/NewBusiness.tsx
@@ -0,0 +1,5 @@
+const NewBusiness = () => {
+  return <div>NewBusiness</div>;
+};
+
+export default NewBusiness;
