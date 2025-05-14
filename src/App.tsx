b45c3acc2779d6cf1d1c9bc145c821d1ae5b8d79
Yosef Hayim <yosefisabag+03@gmail.com>
2025-05-14 21:40:04 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import { Route, Routes } from "react-router";
-import Homepage from "./Pages/Homepage/Homepage";
-import Login from "./Pages/Login/Login";
-import Register from "./Pages/Register/Register";
-import Contact from "./Pages/Contact/Contact";
-import { Pricing } from "./Pages/Pricing/Pricing";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import { PrivacyPolicy } from "./Pages/Privacy";
 
 const App = () => {
   return (
     <>
       <div className="w-full">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route element={<PrivacyPolicy />} path="/privacy" />
         </Routes>
-        <Footer />
       </div>
     </>
   );
