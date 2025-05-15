8c53a2df4f82fc262421da4efb2c07f1bc2d0031
yosefhayim <yosefisabag+03@gmail.com>
2025-05-16 01:28:29 +0300

    ds

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -64,12 +64,9 @@ export const Header = () => {
   return (
     <div className="relative top-0 left-0 mx-auto w-full max-w-7xl px-4 py-20 md:py-40">
       <h1 className="text-2xl font-bold md:text-7xl dark:text-white">
-        The Ultimate <br /> development studio
+        העסקים המובלים בישראל
+        <br /> התקדמו ליומן של קוויקלי
       </h1>
-      <p className="mt-8 max-w-2xl text-base md:text-xl dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks. We are a team of passionate developers and designers that love to build amazing
-        products.
-      </p>
     </div>
   );
 };
