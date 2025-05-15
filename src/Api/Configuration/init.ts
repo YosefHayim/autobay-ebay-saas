74486023cf6d04e1c0f8fea3af2e846bb148fa39
yosefhayim <yosefisabag+03@gmail.com>
2025-05-15 22:39:58 +0300

    d

--- src/Api/Configuration/init.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-interface Database {
-  supabaseUrl: string;
-  supabaseAnonKey: string;
-}
-
-export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
