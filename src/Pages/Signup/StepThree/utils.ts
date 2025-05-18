c56fbb2072a0750c9b8ca7c0dd0b040c5eef7520
yosefhayim <yosefisabag+03@gmail.com>
2025-05-18 14:40:06 +0300

    modified and improve readablitiy

---
+++ src/Pages/Signup/StepThree/utils.ts
@@ -0,0 +1,17 @@
+export const whichCalendarYouHadOptions = [
+  { value: "", text: "בחר יומן" },
+  { value: "physical-calendar", text: "יומן פיזי" },
+  { value: "google-calendar", text: "יומן גוגל" },
+  { value: "other-calendar", text: "מערכת אחרת" },
+  { value: "none", text: "אין לי יומן" },
+];
+
+export const howDidYouHearAboutUsOptions = [
+  { value: "internet-commercial", text: "פרסומת באינטרנט" },
+  { value: "google-commercial", text: "פרסומת בגוגל" },
+  { value: "facebook-commercial", text: "פרסומת בפייסבוק" },
+  { value: "google-search", text: "חיפוש בגוגל" },
+  { value: "recommendation-receive", text: "קיבלתי המלצה" },
+  { value: "appstore-search", text: "חיפוש בחנות אפליקציות" },
+  { value: "other business", text: "ראיתי בעסק אחר" },
+];
