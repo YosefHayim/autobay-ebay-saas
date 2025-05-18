71b8e1a38c2062c5f55f61d948b733302c2dfcdb
yosefhayim <yosefisabag+03@gmail.com>
2025-05-18 17:03:02 +0300

    arrange files

--- src/Pages/Signup/StepFour/StepFour.tsx
+++ src/Pages/Signup/StepFour/StepFour.tsx
@@ -4,6 +4,9 @@ const StepFour = () => {
       <form className="w-1/3">
         <h1>מה סוג העסק שלך?</h1>
         <p>בחר/י את סוג העסק שלך. ניתן לבחור עד 3 סוגי עסקים</p>
+        {businessTypes.map((typeOfB) => (
+          <div key={}></div>
+        ))}
       </form>
     </section>
   );
---
+++ src/Pages/Signup/StepFour/businessArray.tsx
@@ -0,0 +1,155 @@
+import barberIcon from "/svgs/barber-chair-icon.svg";
+import nailGelIcon from "/svgs/nail-gel-icon.svg";
+import eyebrowIcon from "/svgs/eyebrowns-icon.svg";
+import eyelashesIcon from "/svgs/eyelashes-icon.svg";
+import skincareIcon from "/svgs/skin-care-icon.svg";
+import hairDesignIcon from "/svgs/hair-design-icon.svg";
+import hairRemovalIcon from "/svgs/hair-removal-icon.svg";
+import makeupIcon from "/svgs/makeup-icon.svg";
+import tattoIcon from "/svgs/tatto-icon.svg";
+import botoxIcon from "/svgs/botox-icon.svg";
+import massgeIon from "/svgs/massage-icon.svg";
+import dogIcon from "/svgs/dog-icon.svg";
+import hollisticIcon from "/svgs/hollistic-icon.svg";
+import dentistIcon from "/svgs/dentist-icon.svg";
+import glassesIcon from "/svgs/glasses-icon.svg";
+import fitnessIcon from "/svgs/fitness-icon.svg";
+
+export const businessTypes = [
+  {
+    value: "ברבר / מספרת גברים",
+    icon: (
+      <>
+        <img src={barberIcon} alt="barber chair icon" />
+      </>
+    ),
+  },
+  {
+    value: "ציפורניים",
+    icon: (
+      <>
+        <img src={nailGelIcon} alt="nail gel icon" />
+      </>
+    ),
+  },
+  {
+    value: "גבות",
+    icon: (
+      <>
+        <img src={eyebrowIcon} alt="eyebrowns icon" />
+      </>
+    ),
+  },
+  {
+    value: "ריסים",
+    icon: (
+      <>
+        <img src={eyelashesIcon} alt="eyelashes icon" />
+      </>
+    ),
+  },
+  {
+    value: "טיפוח עור",
+    icon: (
+      <>
+        <img src={skincareIcon} alt="skincare icon" />
+      </>
+    ),
+  },
+  {
+    value: "מעצבי שיער",
+    icon: (
+      <>
+        <img src={hairDesignIcon} alt="hair design icon" />
+      </>
+    ),
+  },
+  {
+    value: "הסרת שיער",
+    icon: (
+      <>
+        <img src={hairRemovalIcon} alt="hair removal icon" />
+      </>
+    ),
+  },
+  {
+    value: "איפור",
+    icon: (
+      <>
+        <img src={makeupIcon} alt="makeup icon" />
+      </>
+    ),
+  },
+  {
+    value: "קעקועים ופירסינג",
+    icon: (
+      <>
+        <img src={tattoIcon} alt="barber chair icon" />
+      </>
+    ),
+  },
+  {
+    value: "עיסוי",
+    icon: (
+      <>
+        <img src={massgeIon} alt="massage icon" />
+      </>
+    ),
+  },
+  {
+    value: "הזרקות ומילויים",
+    icon: (
+      <>
+        <img src={botoxIcon} alt="lips icon" />
+      </>
+    ),
+  },
+  {
+    value: "רפואה ורופאי שיניים",
+    icon: (
+      <>
+        <img src={dentistIcon} alt="teeth icon" />
+      </>
+    ),
+  },
+  {
+    value: "מספרת לכלבים",
+    icon: (
+      <>
+        <img src={dogIcon} alt="dog icon" />
+      </>
+    ),
+  },
+  {
+    value: "ייעוץ וטיפול הוליסטי",
+    icon: (
+      <>
+        <img src={hollisticIcon} alt="hollistic threapy icon" />
+      </>
+    ),
+  },
+  {
+    value: "פיטנס",
+    icon: (
+      <>
+        <img src={fitnessIcon} alt="fitness icon" />
+      </>
+    ),
+  },
+  {
+    value: "אופטיקה",
+    icon: (
+      <>
+        <img src={glassesIcon} alt="glasses icon" />
+      </>
+    ),
+  },
+  {
+    value: "אחר",
+    icon: (
+      <>
+        <b>אחר</b>
+      </>
+    ),
+  },
+];
