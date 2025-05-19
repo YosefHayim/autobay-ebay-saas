78863f5ac4757f7f1133df4ad2fcb3968344e21b
yosefhayim <yosefisabag+03@gmail.com>
2025-05-19 21:11:20 +0300

    re

--- src/Pages/Signup/StepFive/WorkingDay/WorkingDay.tsx
+++ src/Pages/Signup/StepFive/WorkingDay/WorkingDay.tsx
@@ -1,7 +1,4 @@
 import WorkingHours from "../WorkingHours/WorkingHours";
-import { Toggle } from "@/components/ui/toggle";
-import { useState } from "react";
-import { MdToggleOn, MdToggleOff } from "react-icons/md";
 
 const hebrewDayMap: Record<string, string> = {
   Sunday: "יום א׳",
@@ -15,29 +12,12 @@ const hebrewDayMap: Record<string, string> = {
 
 const WorkingDay: React.FC<{ day: string }> = ({ day }) => {
   const hebrewDay = hebrewDayMap[day];
-  const [theme, setTheme] = useState<string>("light");
 
   return (
     <div className="flex w-full items-center justify-between py-2">
       <p>{hebrewDay}</p>
       <WorkingHours day={day} isOpen="open" />
       <WorkingHours day={day} isOpen="close" />
-      <Toggle
-        variant="outline"
-        className="group data-[state=on]:hover:bg-muted size-9 data-[state=on]:bg-transparent"
-        pressed={theme === "dark"}
-        onPressedChange={() => setTheme((prev) => (prev === "dark" ? "light" : "dark"))}
-        aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
-      >
-        <MdToggleOn
-          className="shrink-0 scale-0 opacity-0 transition-all group-data-[state=on]:scale-100 group-data-[state=on]:opacity-100"
-          aria-hidden="true"
-        />
-        <MdToggleOff
-          className="absolute shrink-0 scale-100 opacity-100 transition-all group-data-[state=on]:scale-0 group-data-[state=on]:opacity-0"
-          aria-hidden="true"
-        />
-      </Toggle>
     </div>
   );
 };
