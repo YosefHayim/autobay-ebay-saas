6e9079c7a19c95f53652a88a1112cd8e69eef256
yosefhayim <yosefisabag+03@gmail.com>
2025-05-19 10:13:31 +0300

    d

--- src/Pages/Signup/StepFive/StepFive.tsx
+++ src/Pages/Signup/StepFive/StepFive.tsx
@@ -3,15 +3,21 @@ import WorkingDay from "./WorkingDay/WorkingDay";
 const StepFive = () => {
   return (
     <section className="mt-2 flex w-full flex-col items-center justify-center gap-3">
+      <h1 className="text-2xl">מתי אפשר לקבוע לך תורים?</h1>
+      <p>אפשר לשנות את שעות הפעילות גם בהמשך, הפסקות ניתן להגדיר בקלות ביומן.</p>
       <form className="flex w-2/3 flex-col items-start justify-start gap-2">
-        <h1 className="text-2xl">מתי אפשר לקבוע לך תורים?</h1>
-        <p>אפשר לשנות את שעות הפעילות גם בהמשך, הפסקות ניתן להגדיר בקלות ביומן.</p>
         <WorkingDay day="Sunday" />
+        <hr className="w-full" />
         <WorkingDay day="Monday" />
+        <hr className="w-full" />
         <WorkingDay day="Tuesday" />
+        <hr className="w-full" />
         <WorkingDay day="Wednesday" />
+        <hr className="w-full" />
         <WorkingDay day="Thursday" />
+        <hr className="w-full" />
         <WorkingDay day="Friday" />
+        <hr className="w-full" />
         <WorkingDay day="Saturday" />
       </form>
     </section>
