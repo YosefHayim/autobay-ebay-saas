67f20ab12442a38ff2cf247c55079bb77ef65952
yosefhayim <yosefisabag+03@gmail.com>
2025-05-19 15:38:16 +0300

    add toggle functionality

--- src/components/Navbar/menuItems.ts
+++ src/components/Navbar/menuItems.ts
@@ -1,6 +1,6 @@
 export const menuItems = [
   { name: "דף הבית", href: "/" },
-  { name: "מידע ותמיכה", href: "support" },
+  { name: "מידע ותמיכה", href: "/support" },
   { name: "צרו קשר", href: "/contact" },
   { name: "מחירון", href: "/pricing" },
 ];
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import ThemeProvider from "./components/ThemeProvider/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-    ,
-  </BrowserRouter>,
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </ThemeProvider>,
 );
