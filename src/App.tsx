a4b356910811d41c362d87820bb260d6f0d93100
yosefhayim <yosefisabag+03@gmail.com>
2025-05-20 20:12:59 +0300

    working on dahsboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import { Route, Routes } from "react-router";
 import { Home } from "./Pages/Home";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
 import { Pricing } from "./Pages/Pricing";
 import { Contact } from "./Pages/Contact";
 import { SignUp } from "./Pages/SignUp";
 import { Login } from "./Pages/Login";
 import { About } from "./Pages/About";
+import { Dashboard } from "./Pages/Dashboard";
+import { Layout } from "./components/Layout/Layout";
+import { Footer } from "./components/Footer";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        {/* Public routes with layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        {/* Standalone route (no navbar) */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <Footer />
     </>
---
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import DashboardSidebar from "@/components/DashboardSidebar/DashboardSidebar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DashboardSidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
