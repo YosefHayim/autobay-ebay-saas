7a64042af1f26ec7be73d12fb8852f29e0a674f5
yosefhayim <yosefisabag+03@gmail.com>
2025-05-20 19:49:51 +0300

    make logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,14 @@
+import { TextShimmer } from "../ui/text-shimmer";
+
 const Logo = () => {
-  return <p className="cursor-pointer">Bropify.com</p>;
+  return (
+    <TextShimmer
+      duration={3}
+      className="text-xl font-medium [--base-color:theme(colors.gray.600)] [--base-gradient-color:theme(colors.gray.200)] dark:[--base-color:theme(colors.gray.700)] dark:[--base-gradient-color:theme(colors.gray.400)]"
+    >
+      Bropify
+    </TextShimmer>
+  );
 };
 
 export default Logo;
