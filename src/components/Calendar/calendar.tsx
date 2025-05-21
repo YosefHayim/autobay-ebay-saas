bd805411c70301fbe56ca2337f83051d5d7625f3
yosefhayim <yosefisabag+03@gmail.com>
2025-05-21 09:48:19 +0300

    added calendar

---
+++ src/components/Calendar/calendar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CalendarProvider } from "@/modules/calendar/contexts/calendar-context";
+import { CalendarHeader } from "@/modules/calendar/components/header/calendar-header";
+import { CalendarBody } from "@/modules/calendar/components/calendar-body";
+
+import { EventUpdateHandler } from "@/modules/calendar/components/event-update-handler";
+import { DragDropProvider } from "@/modules/calendar/contexts/drag-drop-context";
+import { getEvents, getUsers } from "@/modules/calendar/requests";
+
+async function getCalendarData() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  return {
+    events: await getEvents(),
+    users: await getUsers(),
+  };
+}
+
+export async function Calendar() {
+  const { events, users } = await getCalendarData();
+
+  return (
+    <DragDropProvider>
+      <CalendarProvider events={events} users={users} view="month">
+        <div className="w-full rounded-xl border">
+          <EventUpdateHandler />
+          <CalendarHeader />
+          <CalendarBody />
+        </div>
+      </CalendarProvider>
+    </DragDropProvider>
+  );
+}
---
+++ src/components/Calendar/change-badge-variant-input.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useCalendar } from "@/modules/calendar/contexts/calendar-context";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { DotIcon, PaletteIcon } from "lucide-react";
+import { MotionButton } from "@/modules/calendar/components/header/calendar-header";
+import { buttonHover } from "@/modules/calendar/animations";
+
+export function ChangeBadgeVariantInput() {
+  const { badgeVariant, setBadgeVariant } = useCalendar();
+
+  return (
+    <div className="space-y-1">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <MotionButton
+              variant="outline"
+              size="icon"
+              variants={buttonHover}
+              whileHover="hover"
+              whileTap="tap"
+              onClick={() => setBadgeVariant(badgeVariant === "dot" ? "colored" : "dot")}
+            >
+              {badgeVariant === "dot" ? <DotIcon className="h-5 w-5" /> : <PaletteIcon className="h-5 w-5" />}
+            </MotionButton>
+          </TooltipTrigger>
+          <TooltipContent>Badge variant</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}
