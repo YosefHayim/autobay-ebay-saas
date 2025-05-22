a972b713a8c5fbe41db23d7ca082c262921cf4df
yosefhayim <yosefisabag+03@gmail.com>
2025-05-22 12:20:33 +0300

    s

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import styles from "./loader.module.css";
 
 const Loader = () => {
   return (
-    <div>
+    <div className="flex h-screen w-full items-center justify-center">
       <span className={styles.loader}></span>
     </div>
   );
