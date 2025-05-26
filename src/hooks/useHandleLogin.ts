dd6068446cc4d88a20d14c2b1208930076f83ab4
yosefhayim <yosefisabag+03@gmail.com>
2025-05-26 07:19:28 +0300

    added custom hooks

--- src/hooks/useHandleLogin.ts
+++ src/hooks/useHandleLogin.ts
@@ -1,6 +1,6 @@
 import loginWMobile from "@/Api/loginWMobile";
 
-export const useLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+export const useHandleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   const formData = new FormData(e.currentTarget);
   const phone = formData.get("phone");
