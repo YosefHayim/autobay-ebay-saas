02608a58720276a3b29868283299b63d4596b5df
yosefhayim <yosefisabag+03@gmail.com>
2025-05-30 18:49:46 +0300

    improved css and organize the flow of signup

--- src/Pages/Signup/ChooseBusinessHours/WorkingDay/WorkingDay.tsx
+++ src/Pages/Signup/ChooseBusinessHours/WorkingDay/WorkingDay.tsx
@@ -1,21 +1,9 @@
 import WorkingHours from "../WorkingHours/WorkingHours";
 
-const NameOfDay: Record<string, string> = {
-  sunday: "Sunday",
-  monday: "Monday",
-  tuesday: "Tuesday",
-  wednesday: "Wednesday",
-  thursday: "Thursday",
-  friday: "Friday",
-  saturday: "Saturday",
-};
-
 const WorkingDay: React.FC<{ day: string }> = ({ day }) => {
-  const DayStr = NameOfDay[day];
-
   return (
     <div className="flex w-full items-center justify-between py-2">
-      <p>{DayStr}</p>
+      <p>{day}</p>
       <WorkingHours day={day} isOpen="open" />
       <WorkingHours day={day} isOpen="close" />
     </div>
