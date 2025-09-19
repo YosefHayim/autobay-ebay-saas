e4fcfd34048dcc76e37f15244a6a335a2ace70fe
yosefhayim <yosefisabag+03@gmail.com>
2025-09-20 02:24:35 +0300

    ץ

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
 
@@ -34,7 +34,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         name="search"
         onChange={(e) => handleSearch(e.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+        placeholder={"Search invoices..."}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
