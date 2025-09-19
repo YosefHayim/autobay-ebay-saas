7fcd0ef35d09c17dce528884ca1b5b710c84f767
YosefHayim <yosefisabag@gmail.com>
2025-09-19 15:30:36 +0300

    .

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { Routes,Route } from 'react-router'
+import './App'
+import TermsOfService from './pages/TermsOfService/TermsOfService'
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy'
 
 function App() {
 
   return (
     <>
-    <h1>Placeholder</h1>
+    <Routes>
+      <Route path='/' element={<h1>Welcome</h1>}/>
+      <Route path='terms-of-service' element={<TermsOfService/>}/>
+      <Route path='privacy-policy' element={<PrivacyPolicy/>}/>
+    </Routes>
     </>
   )
 }
